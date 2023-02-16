af5b0124a7f1746ee06cd4d588e80d877109c01f
Uday6747 <96833723+Uday6747@users.noreply.github.com>
2023-02-16 19:52:23 +0530

    An example to implement Command-Line Argument

---
+++ Cla_Exp.java
@@ -0,0 +1,27 @@
+// An example of implementing Command-Line Arguments
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class Cla_Exp
+{
+    public static void main(String args[])
+    {
+        int n = args.length;
+        for(int i = 0; i<n; i++)
+        {
+            System.out.println(args[i]);
+        }
+    }
+}
+
+/*
+Output :-
+D:\Programming\JAVA>javac Cla_Exp.java
+
+D:\Programming\JAVA>java Cla_Exp 1 "A" "Ramu" 10.5
+1
+A   
+Ramu
+10.5
+*/
